da430f604653f614b8f279b02b21fd2055e7175b
Davancimeher <meher.jouini@yahoo.com>
2021-01-31 00:42:06 +0100

    add fx

---
+++ Assets/Scripts/For debuging/PlayerControllerAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerAttack : MonoBehaviour
+{
+    public Animator Animator;
+    bool inAttack = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if(!inAttack)
+            Animator.SetTrigger("Attack1");
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (!inAttack)
+                Animator.SetTrigger("Attack2");
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!inAttack)
+                Animator.SetTrigger("Attack3");
+        }
+    }
+}
